440976de50ae515fc6924fc531d011d573ac74dc
默一 <1254846416@qq.com>
2020-09-27 15:19:06 +0800

    常规更新，新增一个classes/list页面但是暂时没什么鸟用。

---
+++ src/views/classes/list.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card>
+    <v-card-title>class</v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,25 +1,27 @@
 <template>
   <v-container>
-		<v-card color="rgba(255, 255, 255, 0.8)" style="backdrop-filter: blur(2px);">
-			<v-card-title>这是标题</v-card-title>
-			<v-card-text>这是内容啊</v-card-text>
-		</v-card>
+    <v-card color="rgba(255, 255, 255, 0.8)" style="backdrop-filter: blur(2px);">
+      <v-card-title>这是标题</v-card-title>
+      <v-card-text>这是内容啊</v-card-text>
+    </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  
-  mounted: function(){
-		this.redirectLogin()
+  name: "home",
+
+  mounted: function () {
+    this.redirectLogin();
   },
-	methods: {
-		redirectLogin: function(){
-		if(this.$store.state.isLogined == false){
-			this.$router.push("/login");
-		}
-	}
-  }
-}
+  methods: {
+    redirectLogin: function () {
+      if (this.$store.state.isLogined == false) {
+        this.$router.push("/login");
+      } else {
+        this.$router.push("/me");
+      }
+    },
+  },
+};
 </script>
