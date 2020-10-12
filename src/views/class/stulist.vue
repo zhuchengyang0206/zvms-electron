edb41133ef28895ba150b7bc576f6588fd443541
unknown <1254846416@qq.com>
2020-10-12 17:35:08 +0800

    完善学生列表，算法待修改

--- src/views/class/stulist.vue
+++ src/views/class/stulist.vue
@@ -6,12 +6,7 @@
         <template v-slot:activator="{ on: menu, attrs }">
           <v-tooltip bottom>
             <template v-slot:activator="{ on: tooltip }">
-              <v-btn
-                color="white"
-                depressed
-                v-bind="attrs"
-                v-on="{ ...tooltip, ...menu }"
-              >
+              <v-btn color="white" depressed v-bind="attrs" v-on="{ ...tooltip, ...menu }">
                 <div class="headline">{{ nowclassname }}学生列表</div>
               </v-btn>
             </template>
@@ -19,25 +14,13 @@
           </v-tooltip>
         </template>
         <v-list>
-          <v-list-item
-            v-for="(item, index) in classes"
-            :key="index"
-            v-on:click="changeclass(item)"
-          >
-            <v-list-item-title>
-              {{ item.name }}
-            </v-list-item-title>
+          <v-list-item v-for="(item, index) in classes" :key="index" v-on:click="changeclass(item)">
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
       <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="搜索"
-        single-line
-        hide-details
-      ></v-text-field>
+      <v-text-field v-model="search" append-icon="mdi-magnify" label="搜索" single-line hide-details></v-text-field>
     </v-card-title>
     <v-card-text>
       <v-data-table
@@ -64,10 +47,13 @@ export default {
     nowclass: undefined,
     nowclassname: undefined,
     menudisabled: true,
-    dense: false,
     headers: [
       { text: "学号", value: "id", align: "start", sortable: true },
       { text: "姓名", value: "name" },
+      { text: "校内", value: "inside" },
+      { text: "校外", value: "outside" },
+      { text: "大型", value: "large" },
+      { text: "完成", value: "finished" },
     ],
   }),
   mounted: function () {
@@ -109,6 +95,10 @@ export default {
             dialogs.toasts.error(response.data.message);
           else if (response.data.type == "SUCCESS") {
             this.students = response.data.student;
+			//计算义工是否满
+            for(var i=0;i<this.students.length;i++){
+              this.students[i].finished = this.finished(this.students[i])
+            }
           } else dialogs.toasts.error("未知错误");
         })
         .catch((error) => {
@@ -119,10 +109,22 @@ export default {
         });
     },
 
-    changeclass: function (item) {
+    changeclass: function(item) {
       this.nowclass = item.id;
       this.nowclassname = item.name;
       this.fetchstulist();
+    },
+
+    finished: function(item) {
+		var inside = item['inside'];
+		var outside = item['outside'];
+		var large = item['large'];
+		var result = "是";
+		
+		if(outside < 20){this.inside = inside - (20-outside) * 2; outside = 20;}
+		if(inside < 20 || outside < 20 || large < 16 || inside + outside + large - 60 < 4){result = "否";}
+		
+		return result;
     },
   },
 };
