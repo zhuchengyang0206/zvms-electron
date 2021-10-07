5ba258f5c5db5fc5485fbd5a1282b06e3ac43ac3
dblark <dblark@qq.com>
2021-10-07 12:22:06 +0800

    v1.1.0

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,25 @@
 module.exports = {
-  "transpileDependencies": [
+  transpileDependencies: [
     "vuetify"
-  ]
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        "productName": "zvms",
+        "nsis": {
+          "oneClick": false,
+          "allowToChangeInstallationDirectory": true,
+          "createDesktopShortcut": true,
+          "createStartMenuShortcut": true
+        },
+        "win": {
+          "icon": "./public/logo_256x256.png",
+          "target": [{
+            "target": "nsis",
+            "arch": ["ia32", "x64"]
+          }]
+        }
+      },
+    }
+  }
 }
